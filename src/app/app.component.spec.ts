34a1c31d1f203ce4d30b456197ca28767b0232d0
CalmViAda <62995437+CalmViAda@users.noreply.github.com>
2024-03-14 15:52:50 +0100

    [VAC-13] Fix test app component (#4)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,33 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
     declarations: [AppComponent]
-  }));
+  })
+  const fixture = TestBed.createComponent(AppComponent);
+  app = fixture.componentInstance;
+});
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+it('should create the app', () => {
+  expect(app).toBeTruthy();
+});
 
-  it(`should have as title 'vac-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('vac-app');
-  });
+it(`should have as title 'vac-app'`, () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app = fixture.componentInstance;
+  expect(app.title).toEqual('vac-app');
+});
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.div p')?.textContent).toContain('Welcome to VAC');
-  });
+it('should render title', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+  const compiled = fixture.nativeElement as HTMLElement;
+  expect(compiled.querySelector('.content span')?.textContent).toContain('Welcome to VAC');
+});
+
 });
